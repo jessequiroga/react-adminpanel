0a30040023b215aeafa16dd501368935bef81dae
David Wang <devtech324@gmail.com>
2020-05-13 18:20:09 +0200

    ajoput image poche

--- src/model/elements/IconItem.js
+++ src/model/elements/IconItem.js
@@ -1,4 +1,4 @@
 export default {
     "CultMag": {url:`/magazine_de_culte.png`,scaledSize: [70, 70]},
-    "PocheInterdimensionnelle": {url:`/magazine_de_culte.png`,scaledSize: [50, 50]}
+    "PocheInterdimensionnelle": {url:`/poche_interdimentionelle.png`,scaledSize: [70, 70]}
 }
