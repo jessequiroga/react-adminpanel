cacc13779ebc5df4dc5af78b6a8fc001ea6696ee
David Wang <devtech324@gmail.com>
2020-06-12 11:47:21 +0200

    ajout iconBuff invisibilite

--- src/model/elements/IconBuff.js
+++ src/model/elements/IconBuff.js
@@ -2,5 +2,5 @@ export default {
     "CultMag": {url:`/Aveugle.png`,scaledSize: [70, 70]},
     "PocheInterdimensionnelle": {url:`/backpack.png`,scaledSize: [70, 70]},
     "VisionOrb": {url:`/bonus_visibilite.png`,scaledSize: [70, 70]},
-    "DunwichAnnals": {url:`/dunwich_annals.png`,scaledSize: [70, 70]},
+    "DunwichAnnals": {url:`/invisible.png`,scaledSize: [70, 70]},
 }
